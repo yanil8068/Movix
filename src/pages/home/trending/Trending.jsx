c9047981bcb935e3d637ac1973062c773df3efbf
yanil8068 <yanil8068@gmail.com>
2024-05-22 11:22:16 +0530

    Carousel section switching tabs

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-ContentWrapper;
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   );
