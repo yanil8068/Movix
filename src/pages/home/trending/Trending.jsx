0abdc847d35d2b8f8c6028ce6f42626cced9df8e
yanil8068 <yanil8068@gmail.com>
2024-05-22 10:20:41 +0530

    Carousel section

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+ContentWrapper;
+
+const Trending = () => {
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Trending;
