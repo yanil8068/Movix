c5af1fa269b38eb86c13806680dd8d94b68debdc
yanil8068 <yanil8068@gmail.com>
2024-05-23 11:50:28 +0530

    Details Page Videos Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,6 +5,7 @@ import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -17,6 +18,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
